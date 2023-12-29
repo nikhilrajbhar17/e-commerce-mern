6b53eca18b6265ed0b9d482ca2dc3a956942645e
Cold-fish78 <pubg69086@gmail.com>
2023-12-29 17:27:15 +0530

    started

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,23 @@
+import { width } from "@mui/system";
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { MainCarouselData } from "./MainCarouselData";
+
+const items = MainCarouselData.map((item) => (
+  <img
+    src={item.image}
+    style={{ width: "100vw", height: "70vh" }}
+    className="cursor-pointer"
+  />
+));
+
+const MainCarousel = () => (
+  <AliceCarousel
+    items={items}
+    disableButtonsControls
+    autoPlay
+    autoPlayInterval={1000}
+  />
+);
+export default MainCarousel;
---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,23 @@
+export const MainCarouselData = [
+  {
+   
+    image:
+      "https://plus.unsplash.com/premium_photo-1674435578145-4ba6ce1dd95c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1601571115502-83ca3095735b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1505975281173-b339d9fd992c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    image:
+      "https://plus.unsplash.com/premium_photo-1682096159299-5e8a6d5d442b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1479813183133-f2e9b38ed6c4?q=80&w=1423&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
